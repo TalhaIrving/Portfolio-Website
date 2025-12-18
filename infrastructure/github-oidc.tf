c12295c4d000a363e65bf9b68ddc8bc647d197e8
TalhaIrving <talhairving@gmail.com>
2025-12-18 17:01:19 +0000

    feat: updated oidc subject string

--- infrastructure/github-oidc.tf
+++ infrastructure/github-oidc.tf
@@ -22,7 +22,7 @@ resource "aws_iam_role" "github_actions" {
           "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
         }
         StringLike = {
-          "token.actions.githubusercontent.com:sub" = "repo:<ORG>/<REPO>:ref:refs/heads/main"
+          "token.actions.githubusercontent.com:sub" = "repo:TalhaIrving/Portfolio-Website:*"
         }
       }
     }]
